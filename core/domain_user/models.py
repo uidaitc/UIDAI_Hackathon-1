4ace455062ed830690fc69d1ea503e8db78df110
rajarshi07 <rajarshighoshdastidar@gmail.com>
2021-10-27 03:07:46 +0530

    added custom user

--- core/domain_user/models.py
+++ core/domain_user/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
+
 # Create your models here.
 
-
-class CustomUser(AbstractUser):
-    username = None
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
