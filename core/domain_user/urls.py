d3efeaaeada74e9195e1ec402912ad0feaa447f1
Anupam <anupam96786@gmail.com>
2021-10-30 03:48:43 +0530

    check permission added in domain_user

--- core/domain_user/urls.py
+++ core/domain_user/urls.py
@@ -3,8 +3,9 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register'),
-    path('login/', views.loggin, name='login'),
-    path('logout/', LogoutView.as_view(next_page='dashboard'), name='logout'),
+    path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
+    path("login/", views.loggin, name="login"),
+    path("logout/", LogoutView.as_view(next_page="dashboard"), name="logout"),
+    path("check_permission/", views.check_permission, name="check_permission"),
 ]
--- core/domain_user/views.py
+++ core/domain_user/views.py
@@ -1,38 +1,73 @@
-from django.db import models
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-from domain_user.models import CustomUser
+from domain_user.models import CustomUser, Domain
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
 def loggin(request):
     if request.user.is_authenticated:
         return redirect(dashboard)
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
         user = authenticate(username=email, password=password)
         if user is not None:
             print(user)
             login(request, user)
             return redirect(dashboard)
         else:
-            return render(request, 'domain_user/login.html',{'error':'Email or password is incorrect'})
-    return render(request, 'domain_user/login.html',{'error':''})
+            return render(
+                request,
+                "domain_user/login.html",
+                {"error": "Email or password is incorrect"},
+            )
+    return render(request, "domain_user/login.html", {"error": ""})
+
 
 def register(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
         if password == confirm_password:
             try:
                 user = CustomUser.objects.create_user(email, password)
                 user.save()
                 return redirect(loggin)
             except:
-                return render(request, 'domain_user/register.html',{'error':'Email already exists'})
+                return render(
+                    request,
+                    "domain_user/register.html",
+                    {"error": "Email already exists"},
+                )
         else:
-            return render(request, 'domain_user/register.html',{'error':'Password and Confirm Password does not match'})
-    return render(request, 'domain_user/register.html')
+            return render(
+                request,
+                "domain_user/register.html",
+                {"error": "Password and Confirm Password does not match"},
+            )
+    return render(request, "domain_user/register.html")
+
 
 def dashboard(request):
-    return render(request, 'domain_user/dashboard.html')
+    return render(request, "domain_user/dashboard.html")
+
+
+@api_view(["POST"])
+def check_permission(request):
+    domain = Domain.objects.filter(
+        domain=request.data["origin"], domain_key=request.data["apikey"]
+    )
+    if domain:
+        permissions = set(domain[0].permission.split(",")).intersection(
+            set(request.data["permissions"])
+        )
+        return Response(
+            data={"result": True, "permissions": permissions}, status=status.HTTP_200_OK
+        )
+    else:
+        return Response(
+            data={"result": False, "permissions": {}}, status=status.HTTP_200_OK
+        )
